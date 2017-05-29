ddebab74ddb5e979af0041c0e94da53b6cf6b186
Douglas Soares <dsl@cin.ufpe.br>
2017-05-28 21:22:39 -0300

    Testes broadcast - nao testados

--- features/step_definitions/Broadcast_steps.rb
+++ features/step_definitions/Broadcast_steps.rb
@@ -88,13 +88,11 @@ end
 
 When(/^I fill the field "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
   fill_in('arg1', with: arg2)
-
 end
 
 When(/^I fill the field "([^"]*)" with the text "([^"]*)"$/) do |arg1, arg2|
   fill_in('arg1', with: arg2)
   click_button('Send')
-
 end
 
 Then(/^I can see a successful message$/) do
