5f4bbfb6b898ea023fba75381d38b54afda38d85
Rodrigo Cunha <rcs8@cin.ufpe.br>
2017-06-04 20:43:38 +0000

    Set Book validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601192758) do
+ActiveRecord::Schema.define(version: 20170604203706) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "nome"
+    t.integer  "versao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "donations", force: :cascade do |t|
     t.decimal  "amount",        precision: 12, scale: 2
