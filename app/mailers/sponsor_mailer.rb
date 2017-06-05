93ba919384576ecb820ff9e6cf367e808ee006ec
Douglas Soares <dslins@outlook.com>
2017-06-05 00:25:43 +0000

    Fix Sponsor mailer method

--- app/mailers/sponsor_mailer.rb
+++ app/mailers/sponsor_mailer.rb
@@ -4,7 +4,7 @@ class SponsorMailer < ApplicationMailer
   def thanks_email (sponsor)
     @sponsor = sponsor
 
-    attachments.inline['ibraema_logo.jpg'] = File.read('#{Rails.root}/app/assets/images/ibraema_logo.jpg')
+    attachments.inline['ibraema_logo.jpg'] = File.read('app/assets/images/ibraema_logo.jpg')
     mail to: sponsor.email, subject: "Obrigado por nos ajudar a lutar contra o analfabetismo"
   end
 end
