007e4e98a4d526cb41bda6269eed739d8942bf8c
Ramon Saboya <ramons.gomes@hotmail.com>
2017-06-06 19:42:58 +0000

    Create navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :people
   resources :users
   resources :donations
+  root 'people#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
