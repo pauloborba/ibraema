08ed8fb1d9374fdf69d13fc26dd65d99fcce5aba
Douglas Soares <dslins@outlook.com>
2017-06-13 01:47:45 +0000

    Add validations and methods to Emails model

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,18 @@
 class Email < ApplicationRecord
+    before_save :check_blank_subject, :check_mark
+    validates :subject, presence: true
+    validates :message, presence: true
+    
+    def check_mark
+        self.mark = true
+    end
+
+    def check_blank_subject
+        return false if self.subject.blank? == true
+    end
+    
+    def count_resend
+        self.resent = self.resent + 1
+    end
+    
 end
